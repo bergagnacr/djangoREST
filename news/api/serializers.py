4d5e0d8a6520f1946d8c27a4d80c56405c1640fb
Cristian Sarmiento <cristian.sarmiento@darwoft.com>
2021-05-18 16:13:44 -0300

    add validators

--- news/api/serializers.py
+++ news/api/serializers.py
@@ -26,3 +26,13 @@ class ArticleSerializer(serializers.Serializer):
         instance.active = validated_data.get('active', instance.active)
         instance.save()
         return instance
+
+    def validate(self, data):
+        """ check that description and title are different"""
+        if data["title"] == data["description"]:
+            raise serializers.ValidationError("Title and description must be different from one another")
+        return data
+
+    def validate_title(self, value):
+        if len(value) < 60:
+            raise serializers.ValidationError("The title must have at least 60 characters")
